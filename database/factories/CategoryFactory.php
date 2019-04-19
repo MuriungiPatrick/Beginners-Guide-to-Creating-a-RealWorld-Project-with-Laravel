4f6c170b0fa295131f03dae292e1728832af4ee7
Patrick Muriungi <muriungipatrick@gmail.com>
2019-04-19 21:23:14 +0300

    Added Faker for Categories Table

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Model::class, function (Faker $faker) {
     return [
-        //
+        'title' =>$faker->sentence(rand(2, 3), true),
+        'description' =>$faker->words(rand(2, 3), true),
+        'slug' =>$faker->unique()->slug,
+        'updated_at' =>now(),
+        'created_at' =>now()
     ];
 });
