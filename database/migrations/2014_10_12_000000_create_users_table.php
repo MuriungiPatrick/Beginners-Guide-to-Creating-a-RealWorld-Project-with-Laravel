b5a509e907ac0dda0e20520ced3b2cbce7906858
Patrick Muriungi <muriungipatrick@gmail.com>
2019-05-21 15:26:00 +0300

    Modified the Jobs Table, Users Table and Categories Table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+      Schema::disableforeignKeyConstraints();
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->Increments('id');
             $table->string('name');
+            $table->string('slug')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
